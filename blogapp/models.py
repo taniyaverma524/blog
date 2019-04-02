f8a6d5b93931366f84f7f8f93d9a4f6fff48193d
Taniya Verma <inimitabletaniya14@gmail.com>
2019-04-02 17:43:35 +0530

    commit

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from django.utils.text import slugify
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=32)
+    slug = models.SlugField(max_length=32,unique=True)
+    body = models.TextField()
+    date = models.DateField(auto_created=True)
+    def __str__(self):
+        return self.title
+
+    def half(self):
+        return self.body[:50] + "...."
+    def slug(self):
+
+        return slugify(self.title)
+
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='blogs', related_query_name='blog')
+    comment = models.CharField(max_length=512)
+    created = models.DateTimeField(auto_created=True)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.blog.title
